e14082cabc7661a319ca4f854763490eb20e4120
WEB_APIDeveloper <elthrone1233@gmail.com>
2025-01-11 13:55:45 +0800

    Update _app-7c82081751637b18.js

--- _next/static/chunks/pages/_app-7c82081751637b18.js
+++ _next/static/chunks/pages/_app-7c82081751637b18.js
@@ -388,7 +388,7 @@
         branding: "/#branding",
         play: "/#play",
         treasury: "https://dexscreener.com/solana/0x00000000000",
-        demos: "https://app.gen-ai.website/",
+        demos: "https://app.gen-ai.us.com",
       };
     },
     1896: (e, t, r) => {
